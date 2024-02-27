2999735a962f64298270349d026e0218a4a77355
Rachel Prather <rachelprather1@gmail.com>
2024-02-27 15:36:23 -0800

    Create mock data set

---
+++ test/testData.js
@@ -0,0 +1,293 @@
+export default {
+  users: [
+    {
+      "id": 1,
+      "name": "Trystan Gorczany",
+      "address": "9484 Lucas Flat, West Kittymouth WA 67504",
+      "email": "Taurean_Pollich31@gmail.com",
+      "strideLength": 4,
+      "dailyStepGoal": 7000,
+      "friends": [
+        5,
+        43,
+        46,
+        11
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Tyreek VonRueden",
+      "address": "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      "email": "Nicolette_Halvorson43@yahoo.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 9000,
+      "friends": [
+        13,
+        19,
+        3
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Colt Rohan",
+      "address": "48010 Balistreri Harbor, Cleobury IN 43317",
+      "email": "Wilford.Barton@gmail.com",
+      "strideLength": 2.7,
+      "dailyStepGoal": 3000,
+      "friends": [
+        31,
+        16,
+        15,
+        7
+      ]
+    },
+    {
+      "id": 4,
+      "name": "Evie Satterfield",
+      "address": "1378 Renner Island, Port Lincoln NE 06237-3602",
+      "email": "Adan66@yahoo.com",
+      "strideLength": 3.9,
+      "dailyStepGoal": 4000,
+      "friends": [
+        21,
+        32,
+        8
+      ]
+    },
+    {
+      "id": 5,
+      "name": "Brycen Rutherford",
+      "address": "0770 Keeley Square, West Keyon SD 73400-6577",
+      "email": "Jerald55@yahoo.com",
+      "strideLength": 3.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        5,
+        46
+      ]
+    },
+    {
+      "id": 6,
+      "name": "Jillian Senger",
+      "address": "235 Geoffrey Highway, New Lavadaport CA 62933-3709",
+      "email": "Cortez51@gmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        25,
+        15,
+        20
+      ]
+    },
+    {
+      "id": 7,
+      "name": "Elaina Mosciski",
+      "address": "307 Gerhard Pine, Luettgenton NC 60477",
+      "email": "Karolann.Kuvalis@yahoo.com",
+      "strideLength": 3,
+      "dailyStepGoal": 9000,
+      "friends": [
+        7,
+        46,
+        43
+      ]
+    },
+    {
+      "id": 8,
+      "name": "Amir Lang",
+      "address": "75165 Zulauf Walks, North Dejaview NC 21971-9115",
+      "email": "Eudora.Stracke@yahoo.com",
+      "strideLength": 2.8,
+      "dailyStepGoal": 2000,
+      "friends": [
+        46,
+        8,
+        29,
+        33,
+        10
+      ]
+    },
+    {
+      "id": 9,
+      "name": "Antonina McClure",
+      "address": "480 Niko Village, Thompsonchester GA 94178-0548",
+      "email": "Dustin.Kris15@yahoo.com",
+      "strideLength": 3.5,
+      "dailyStepGoal": 11000,
+      "friends": [
+        26,
+        7,
+        39,
+        3
+      ]
+    }
+  ],
+  hydrationData: [
+    {
+      "userID": 1,
+      "date": "2023/03/24",
+      "numOunces": 28
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 35
+    },
+    {
+      "userID": 3,
+      "date": "2023/03/24",
+      "numOunces": 95
+    },
+    {
+      "userID": 4,
+      "date": "2023/03/24",
+      "numOunces": 74
+    },
+    {
+      "userID": 5,
+      "date": "2023/03/24",
+      "numOunces": 47
+    },
+    {
+      "userID": 6,
+      "date": "2023/03/24",
+      "numOunces": 86
+    },
+    {
+      "userID": 7,
+      "date": "2023/03/24",
+      "numOunces": 74
+    },
+    {
+      "userID": 8,
+      "date": "2023/03/24",
+      "numOunces": 36
+    },
+    {
+      "userID": 9,
+      "date": "2023/03/24",
+      "numOunces": 40
+    }
+  ],
+  sleep: [
+    {
+    "userID": 1,
+    "date": "2023/03/24",
+    "hoursSlept": 9.6,
+    "sleepQuality": 4.3
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/24",
+    "hoursSlept": 8.4,
+    "sleepQuality": 3.5
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/24",
+    "hoursSlept": 9.7,
+    "sleepQuality": 4.7
+    },
+    {
+    "userID": 4,
+    "date": "2023/03/24",
+    "hoursSlept": 4.7,
+    "sleepQuality": 3
+    },
+    {
+    "userID": 5,
+    "date": "2023/03/24",
+    "hoursSlept": 8,
+    "sleepQuality": 3.1
+    },
+    {
+    "userID": 6,
+    "date": "2023/03/24",
+    "hoursSlept": 4.2,
+    "sleepQuality": 1.2
+    },
+    {
+    "userID": 7,
+    "date": "2023/03/24",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.9
+    },
+    {
+    "userID": 8,
+    "date": "2023/03/24",
+    "hoursSlept": 9.2,
+    "sleepQuality": 1.6
+    },
+    {
+    "userID": 9,
+    "date": "2023/03/24",
+    "hoursSlept": 4.8,
+    "sleepQuality": 2.5
+    }
+  ],
+  activity: [
+    {
+    "userID": 1,
+    "date": "2023/03/24",
+    "numSteps": 7362,
+    "minutesActive": 261,
+    "flightsOfStairs": 26
+    },
+    {
+    "userID": 2,
+    "date": "2023/03/24",
+    "numSteps": 3049,
+    "minutesActive": 125,
+    "flightsOfStairs": 43
+    },
+    {
+    "userID": 3,
+    "date": "2023/03/24",
+    "numSteps": 12970,
+    "minutesActive": 282,
+    "flightsOfStairs": 38
+    },
+    {
+    "userID": 4,
+    "date": "2023/03/24",
+    "numSteps": 8934,
+    "minutesActive": 294,
+    "flightsOfStairs": 19
+    },
+    {
+    "userID": 5,
+    "date": "2023/03/24",
+    "numSteps": 8443,
+    "minutesActive": 136,
+    "flightsOfStairs": 43
+    },
+    {
+    "userID": 6,
+    "date": "2023/03/24",
+    "numSteps": 13297,
+    "minutesActive": 116,
+    "flightsOfStairs": 13
+    },
+    {
+    "userID": 7,
+    "date": "2023/03/24",
+    "numSteps": 7765,
+    "minutesActive": 74,
+    "flightsOfStairs": 31
+    },
+    {
+    "userID": 8,
+    "date": "2023/03/24",
+    "numSteps": 7255,
+    "minutesActive": 42,
+    "flightsOfStairs": 28
+    },
+    {
+    "userID": 9,
+    "date": "2023/03/24",
+    "numSteps": 3801,
+    "minutesActive": 41,
+    "flightsOfStairs": 20
+    }
+  ]
+}
