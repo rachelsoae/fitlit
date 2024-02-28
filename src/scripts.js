0593438463505b29b5da9577e90e911fd51c3991
Rachel Prather <rachelprather1@gmail.com>
2024-02-27 17:06:09 -0800

    Return user data

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,12 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+const getUser = (dataset, id) => {
+  return dataset.find(user => user.id === id)
+}
 
+// const averageStepGoal = (dataset) => {
+//   return dataset.reduce(())
+// }
 
-// An example of how you tell webpack to use a CSS file
-import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
-
-// An example of how you tell webpack to use a JS file
-import userData from './data/users';
-console.log("User Data:", userData);
-
-// Example of one way to import functions from the domUpdates file.  You will delete these examples.
-import { exampleFunction1, exampleFunction2 } from './domUpdates';
-
-exampleFunction1('Travis');
-exampleFunction2('Travis')
+export {
+  getUser,
+  // averageStepGoal
+};
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,27 @@
 import { expect } from 'chai';
+import '../src/data/users.js';
+import { 
+  getUser, 
+  // averageStepGoal 
+} from '../src/scripts.js';
+import testData from './testData.js';
 
-describe('User Repository', () => {
-  it('should run tests', function () {
-    expect(true).to.equal(true);
+const users = testData.users;
+describe('User Data', () => {
+  it('should return a user\'s data, given their ID', () => {
+    const id = 9;
+    const user = getUser(users, id);
+    expect(user).to.equal(users[8])
   });
+
+  it('should be able to return a different user', () => {
+    const id = 3;
+    const user = getUser(users, id);
+    expect(user).to.equal(users[2])
+  })
+
+  // it('should calculate the average step goal for a set of users', () => {
+  //   const groupGoal = averageStepGoal(users)
+  //   expect(groupGoal).to.equal(6500)
+  // })
 });
